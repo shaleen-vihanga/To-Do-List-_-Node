30a3453422e37c2557345b607186673a3976595b
Shaleen Perera <shaleenvihanga@gmail.com>
2024-03-29 11:27:17 +0530

    Mongodb connection established

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const TodoTask = require("./models/TodoTask");
+
+dotenv.config();
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/static", express.static("public"));
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+.then(() => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+})
+.catch((err) => { console.error(err); });
 
 app.set("view engine", "ejs");
 
@@ -11,8 +24,17 @@ app.get('/', (req,res) => {
     res.render('todo.ejs');
 });
 
-app.post('/',(req,res) => {
-    console.log(req.body)
+app.post('/',async (req,res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.send(500, err);
+        res.redirect("/");
+    }
 })
 
-app.listen(3000, () => console.log("Server Up and running"));
+// app.listen(3000, () => console.log("Server Up and running"));
